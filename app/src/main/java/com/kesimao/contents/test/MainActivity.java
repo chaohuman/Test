e1c04a5997c478ad74f2de24310fbc9c84d1546c
chaohuman <775449080@qq.com>
2016-11-07 20:14:17 +0800

    Initial commit

---
+++ app/src/main/java/com/kesimao/contents/test/MainActivity.java
@@ -0,0 +1,38 @@
+package com.kesimao.contents.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        tv = (TextView) findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
+        Button button_go = (Button) findViewById(R.id.button_go);
+        button_go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv.setText("世界，你好！");
+            }
+        });
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+}
